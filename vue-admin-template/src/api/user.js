bbb8f02476d81a631cb5a7447426ac20f9319811
kidword <1564636097@qq.com>
2020-07-13 11:56:52 +0800

    init

---
+++ vue-admin-template/src/api/user.js
@@ -0,0 +1,33 @@
+import request from '@/utils/request'
+
+// 身份认证
+export function Auto(data) {
+  return request({
+    url: '/vue-admin-template/user/login',
+    method: 'post',
+    data
+  })
+}
+
+export function login(data) {
+  return request({
+    url: '/vue-admin-template/user/login',
+    method: 'post',
+    data
+  })
+}
+
+export function getInfo(token) {
+  return request({
+    url: '/vue-admin-template/user/info',
+    method: 'get',
+    params: { token }
+  })
+}
+
+export function logout() {
+  return request({
+    url: '/vue-admin-template/user/logout',
+    method: 'post'
+  })
+}
